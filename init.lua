c65e248531780cebabc4c89eabdc5c20bb0af2fc
Oscar Joel Ramírez Manzano <oramirez78203@gmail.com>
2026-08-21 04:35:41 -0500

    style: explicitly load tokyonight-night colorscheme

--- init.lua
+++ init.lua
@@ -66,7 +66,7 @@ require("lazy").setup({
         transparent = false,
         terminal_colors = true,
       })
-      vim.cmd.colorscheme('tokyonight')
+      vim.cmd.colorscheme('tokyonight-night')
     end,
   },
   {
